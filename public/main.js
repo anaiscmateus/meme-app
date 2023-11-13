eaea03ed4813fdf2efcf1ae020f55f0dcd4c2c7e
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-11-13 01:16:38 -0500

    correcting files

--- public/main.js
+++ public/main.js
@@ -1,45 +1,46 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
+var trash = document.getElementsByClassName("trash");
+var heart = document.getElementsByClassName("heart");      
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
+// handle favorites
+Array.from(heart).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const imgSrc = this.parentNode.parentNode.parentNode.childNodes[1].src
+    console.log(imgSrc)
+    const url = imgSrc.substring(imgSrc.lastIndexOf('/') + 1);
+    console.log(url)
+    fetch('favorite', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'url': url,
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
 });
 
+// Handle deletes
 Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
+  element.addEventListener('click', function(){
+    const imgSrc = this.parentNode.parentNode.querySelector('img').getAttribute('src');
+    const url = imgSrc.substring(imgSrc.lastIndexOf('/') + 1);
+
+    fetch('delete', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'url': url,
+      })
+    }).then(function (response) {
+      window.location.reload()
+    })
+  });
 });
--- server.js
+++ server.js
@@ -1,61 +1,104 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-var db, collection;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://anaismateusc:kZLShfiGM6j8o8mu@memes.aweyd4e.mongodb.net/memes?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.listen(3000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
-    });
+// Create a Mongoose model for memes
+const Meme = mongoose.model('Meme', {
+    url: String,
+    favorites: {
+        type: Number,
+        default: 0,
+    },
 });
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(express.static('public'))
+// Setup Multer for file uploads
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
 
-app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
-  })
-})
+const upload = multer({ storage });
 
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
+// Use bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
-    if (err) return res.send(500, err)
-    res.send('Message deleted!')
-  })
-})
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/', async (req, res) => {
+  const memes = await Meme.find();
+  res.render('index', { memes }); // Pass memes data to the template
+});
+
+// Handle meme uploads
+app.post('/upload', upload.single('meme'), async (req, res) => {
+  const meme = new Meme({
+      url: `/uploads/${req.file.filename}`,
+      favorites: 0
+});
+
+  await meme.save();
+  res.redirect('/');
+});
+
+// Handle favorites
+app.put('/favorite', async (req, res) => {
+  try {
+      const clientUrl = req.body.url;
+      const dbUrl = `/uploads/${clientUrl}`;
+
+      const updatedMeme = await Meme.findOneAndUpdate({ url: dbUrl }, { $inc: { favorites: 1 } }, { new: true });
+
+      if (!updatedMeme) {
+          return res.status(404).send('Meme not found');
+      }
+
+      res.status(200).json({ favorites: updatedMeme.favorites });
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
+// handle deletes
+app.delete('/delete', async (req, res) => {
+  try {
+      const clientUrl = req.body.url;
+      const dbUrl = `/uploads/${clientUrl}`;
+
+      const deletedMeme = await Meme.findOneAndDelete({ url: dbUrl }).exec();
+
+      if (!deletedMeme) {
+          return res.status(404).send('Meme not found');
+      }
+
+      // You might want to delete the file from the server too. Add this if needed:
+      // const filePath = path.join(__dirname, 'public', 'uploads', clientUrl);
+      // fs.unlinkSync(filePath);
+
+      res.status(200).send('Meme deleted successfully');
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
